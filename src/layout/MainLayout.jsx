a61fa73d430425af2eead795c208446691b8b2ae
hanhtran <gm.tranhuuhanh@gmail.com>
2023-03-13 12:13:18 -0700

    start position menu commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
-import { Breadcrumb, Layout, Menu, Image, Space, notification } from "antd";
-import { LogoutOutlined, } from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, Image, Space, notification, Avatar } from "antd";
 import Icon from "@mdi/react";
-import { mdiAccount, mdiAccountCash, mdiFileEdit, mdiClockTimeFour, mdiTicketAccount, mdiCalendarMonth, mdiAccountNetwork, mdiAccountSupervisor } from "@mdi/js";
+import { mdiAccount, mdiAccountCash, mdiFileEdit, mdiClockTimeFour, mdiTicketAccount, mdiCalendarMonth, mdiAccountNetwork, mdiAccountSupervisor, mdiLogout } from "@mdi/js";
 import { Outlet, useNavigate } from 'react-router-dom';
 import { setUserInfor } from '../store/actions/user.action';
 import { useDispatch } from 'react-redux';
 const { Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children, type) {
+function getItem(label, key, icon, children, type, disabled) {
     return {
         key,
         icon,
         children,
         label,
         type,
+        disabled
     };
 }
 const items = [
@@ -22,16 +22,17 @@ const items = [
     getItem('Daily Task', '/tomorrow2', <Icon path={mdiFileEdit} size={1} />),
     getItem('Time Sheet', '/tomorrow3', <Icon path={mdiClockTimeFour} size={1} />),
     getItem('Payroll', '/tomorrow4', <Icon path={mdiAccountCash} size={1} />),
-    getItem('Ticket', '/tomorrow5', <Icon path={mdiTicketAccount} size={1} />, 
-    [
-        getItem('Miss Punch', '/admin/ticket/miss-punch'),
-        getItem('Time Off', '/admin/ticket/time-off'),
-    ]),
+    getItem('Ticket', '/tomorrow5', <Icon path={mdiTicketAccount} size={1} />,
+        [
+            getItem('Miss Punch', '/admin/ticket/miss-punch'),
+            getItem('Time Off', '/admin/ticket/time-off'),
+        ]),
     getItem('DEPARTMENT'),
     getItem('Calendar', '/tomorrow6', <Icon path={mdiCalendarMonth} size={1} />),
     getItem('TO-CoWorker', '/tomorrow7', <Icon path={mdiAccountNetwork} size={1} />),
     getItem('Manager', '/tomorrow8', <Icon path={mdiAccountSupervisor} size={1} />),
-    getItem('Log Out', 'logOut', <LogoutOutlined />),
+    getItem(<hr color='white' />, null, null, null, null, true),
+    getItem('Log Out', 'logOut', <Icon path={mdiLogout} size={1} />),
 
 
     // getItem('User Management', '/project-management/user', <TeamOutlined />),
@@ -58,17 +59,13 @@ export default function MainLayout() {
                 <Sider collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} collapsible>
                     <div className="logo" >
                         <a href="/">
-
-                            {collapsed &&
-                                <div className="sideBar-icon-sm">
-                                    <img src="https://admin.worldcraftlogistics.net/images/Logo_no_bg.png" height={50} alt="" />
-                                </div>
-                            }
-                            {!collapsed &&
-                                <div className="sideBar-icon-lg">
-                                    <img src="https://admin.worldcraftlogistics.net/images/Logo__001_no_bg.png" height={50} alt="" />
-                                </div>
-                            }
+                            <div className={`sideBar-icon-sm ${!collapsed ? "hiden_logo" : ""}`}>
+                                <img src="https://admin.worldcraftlogistics.net/images/Logo_no_bg.png" height={50} alt="" />
+                            </div>
+                            <div className={`sideBar-icon-lg ${collapsed ? "hiden_logo" : ""}`}>
+                                <img src="https://admin.worldcraftlogistics.net/images/Logo__001_no_bg.png" height={50} alt="" />
+                            </div>
+                            {/* } */}
                         </a>
                     </div>
                     <Menu
@@ -87,8 +84,44 @@ export default function MainLayout() {
                         }}
                     />
                 </Sider>
-                <Layout className="site-layout">
-                    <Space direction="vertical">
+                <Layout className="site-layout" 
+                style={{marginLeft:'80px'}}
+                >
+                    <Space direction=" horizontal">
+
+                        <Menu
+                            mode="inline"
+                            style={{
+                                backgroundColor: '#f4f5f7',
+                            }}
+                            onClick={({ key }) => {
+                                navigate(key)
+                            }}
+                            items={[
+                                {
+                                    label: 'Welcome !',
+                                    icon: <Avatar src="https://admin.worldcraftlogistics.net/img/Avatar/hanhT__2023-03-04-01-23-53-990.jpg" width={100} preview={false} />,
+                                    disabled: false,
+                                    children: [
+                                        { label: 'Change Avatar', icon: <i className="fa-solid fa-truck"></i>, disabled: true },
+                                        { label: 'Change Password', icon: <i className="fa-solid fa-equals"></i>, disabled: true },
+                                        { label: 'Support', icon: <i className="fa-solid fa-paste"></i>, disabled: true },
+                                    ],
+                                },
+                                { label: <hr />, disabled: true },
+                                // { label: 'Cyber Board', key: '/project-management/board', icon: <i className="fa-solid fa-credit-card"></i> },
+                                // { label: 'Project Settings', icon: <i className="fa-solid fa-gear"></i>, disabled: true },
+                                // { label: <hr />, disabled: true },
+                                // { label: 'Releases', icon: <i className="fa-solid fa-truck"></i>, disabled: true },
+                                // { label: 'Issues and Filters', icon: <i className="fa-solid fa-equals"></i>, disabled: true },
+                                // { label: 'Pages', icon: <i className="fa-solid fa-paste"></i>, disabled: true },
+                                // { label: 'Reports', icon: <i className="fa-solid fa-location-arrow"></i>, disabled: true },
+                                // { label: 'Components', icon: <i className="fa-solid fa-box"></i>, disabled: true },
+                            ]}
+                        >
+
+                        </Menu>
+
                         <Content
                             style={{
                                 margin: '0 0.5rem',
--- src/pages/Ticket/MissPunch/MissPunchForm.jsx
+++ src/pages/Ticket/MissPunch/MissPunchForm.jsx
@@ -1,7 +1,269 @@
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory,
+{
+  PaginationProvider,
+  SizePerPageDropdownStandalone,
+  PaginationTotalStandalone,
+  PaginationListStandalone,
+} from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
+
+import '../TimeOff/dataTableStyle.scss'
+import Icon from '@mdi/react';
+import {
+  mdiMenuUp,
+  mdiMenuDown,
+  mdiCheckCircleOutline,
+  mdiCloseCircleOutline,
+  mdiDotsHorizontalCircleOutline
+} from '@mdi/js';
+import { Space } from 'antd';
+const { SearchBar } = Search;
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+  if (i % 2 === 0) {
+    data.push(
+      {
+        reference: 'MSF' + [i],
+        submitData: '03/02/2023',
+        manager: ['Huy Nguyen'],
+        managerDate: '01/02/2023',
+        managerStatus: true,
+        hrDate: '01/02/2023',
+        hrStatus: true,
+      },
+    );
+  }
+  else {
+    data.push(
+      {
+        reference: 'MSF' + [i],
+        submitData: '03/02/2023',
+        manager: ['Khanh Nguyen'],
+        managerDate: '01/02/2023',
+        managerStatus: false,
+        hrDate: '01/02/2023',
+        hrStatus: '...',
+      },
+    );
+  }
+}
+
+const options = {
+  // prePageText: "",
+  // nextPageText: '',
+  // hidePageListOnlyOnePage: false,
+  showTotal: false,
+  paginationSize: 4,
+  alwaysShowAllBtns: true,
+  withFirstAndLast: false,
+  custom: true,
+  totalSize: data.length,
+  alwaysShowAllBtns: true,
+  sizePerPageList: [
+    {
+      text: '10', value: 10
+    },
+    {
+      text: '20', value: 20
+    },
+    {
+      text: '30', value: 30
+    },
+    {
+      text: '40', value: 40
+    },
+    {
+      text: '50', value: 50
+    },
+    {
+      text: 'All', value: data.length
+    }
+  ],
+  // A numeric array is also available. the purpose of above example is custom the text
+};
+///// function component for sort icon block
+function IconSort(props) {
+  return (
+    <Space direction='vertical' >
+      <Icon path={mdiMenuUp}
+        color={`${props.up && props.active ? '#096481' : '#d0d2d4'}`}
+        size={1}
+      />
+      <Icon path={mdiMenuDown}
+        color={`${props.down && props.active ? '#096481' : '#d0d2d4'}`}
+        size={1}
+      />
+    </Space>
+  )
+
+}
+const customSort = (order, column) => {
+  if (!order) return (<IconSort active={false} up={false} down={false} />)
+  else if (order === 'asc') return (<IconSort active={true} up={true} down={false} />)
+  else if (order === 'desc') return (<IconSort active={true} up={false} down={true} />)
+  return null;
+}
+///// function component for sort next/prev pagination
+
+// const pageButtonRenderer = ({
+//   title,
+// }) => {
+//   const titleStyle = {}
+//   const nextAndPrevIcon = {
+//     if(title) {
+
+//     }
+//   }
+// }
+
+function referenceFormatter(cell, row) {
+  if (row.hrStatus === true) {
+    return (
+      <span>
+        <a href='#'>{cell}</a>
+      </span>
+    );
+  }
+  return (
+    <span> {cell}</span>
+  );
+}
+
+function StatusFormatter(cell, row) {
+  return (
+    < span >
+      {(cell == '...')
+        ? <Icon path={mdiDotsHorizontalCircleOutline} size={0.8} color='gray' />
+        : (
+          (cell == false)
+            ? <Icon path={mdiCloseCircleOutline} size={0.8} color='red' />
+            : <Icon path={mdiCheckCircleOutline} size={0.8} color='green' />
+        )
+      }
+    </span >
+  );
+}
+
+
+const columns = [{
+  dataField: 'reference',
+  text: 'Reference',
+  sort: true,
+  formatter: referenceFormatter,
+  sortCaret: customSort,
+}, {
+  dataField: 'submitData',
+  text: 'Submit Data',
+  sort: true,
+  sortCaret: customSort,
+
+}, {
+  dataField: 'manager',
+  text: 'Manager',
+  sort: true,
+  sortCaret: customSort,
+}, {
+  dataField: 'managerDate',
+  text: 'Manager Date',
+  sort: true,
+  sortCaret: customSort,
+}, {
+  dataField: 'managerStatus',
+  text: 'Manager Status',
+  sort: true,
+  formatter: StatusFormatter,
+  sortCaret: customSort,
+}, {
+  dataField: 'hrDate',
+  text: 'HR Date',
+  sort: true,
+  sortCaret: customSort,
+}, {
+  dataField: 'hrStatus',
+  text: 'HR Status',
+  sort: true,
+  formatter: StatusFormatter,
+  sortCaret: customSort,
+}
+];
 
 export default function MissPunchForm() {
+
   return (
-    <div>MissPunchForm</div>
+    <Space direction="vertical" className='d-flex dataTable'>
+      <PaginationProvider pagination={paginationFactory(options)}>
+        {
+          ({
+            paginationProps,
+            paginationTableProps
+          }) => (
+            <>
+
+              <ToolkitProvider
+                keyField="id"
+                data={data}
+                columns={columns}
+                search
+              >
+                {
+                  toolkitprops => (
+                    <Space direction='vertical d-dlex w-100'>
+                      <Space direction='horizontal' align='baseline' className='justify-content-between d-flex'>
+                        <Space>
+                          <span>Show</span>
+                          <SizePerPageDropdownStandalone {...paginationProps} />
+                          <span>entries</span>
+                        </Space>
+                        <Space >
+                          <SearchBar {...toolkitprops.searchProps} />
+                        </Space>
+                      </Space>
+
+                      <hr />
+                      <BootstrapTable
+                        bootstrap4
+                        keyField='id'
+                        data={data}
+                        columns={columns}
+                        bordered={false}
+                        {...toolkitprops.baseProps}
+                        {...paginationTableProps}
+                      />
+                      {
+                        (data.length == 0) &&
+                        <Space direction='vertical' className='d-dlex text-center w-100' >
+                          <p className='pt-3'>No data available in table</p>
+                          <hr />
+                        </Space>
+                      }
+                    </Space>
+                  )
+                }
+              </ToolkitProvider>
+              <Space direction='horizontal' align='baseline' className='justify-content-between d-flex'>
+                <PaginationTotalStandalone
+                  {...paginationProps}
+                />
+                {(data.length == 0) ?
+                  <Space>
+                    <ul class="pagination react-bootstrap-table-page-btns-ul">
+                      <li class="disabled page-item" title="previous page"><a href="#" class="page-link">&lt;</a></li>
+                      <li class="disabled page-item" title="next page"><a href="#" class="page-link">&gt;</a></li>
+                    </ul>
+                  </Space>
+
+                  : <PaginationListStandalone
+                    {...paginationProps}
+                  />
+                }
+              </Space>
+            </>
+          )
+        }
+      </PaginationProvider>
+    </Space >
   )
 }

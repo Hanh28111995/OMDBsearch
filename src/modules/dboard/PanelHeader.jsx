e37b24a0f1f9856a3abc6ae32059db962ed01d22
hanhtran <gm.tranhuuhanh@gmail.com>
2023-04-03 09:38:43 -0700

    last style & deploy  page commit

--- src/modules/dboard/PanelHeader.jsx
+++ src/modules/dboard/PanelHeader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function PanelHeader(props) {
   return (
-    <div style={{ display:'flex',flexDirection:'row', justifyContent:'space-between'}}>
-        <div>{props.name}</div>
-        <div style={{ display:'flex',flexDirection:'row', justifyContent:'space-between'}}>
-            <div className='mr-3'>15 <i className="fa-solid fa-thumbs-up"></i></div>    
-            <div className='mr-3'>15 <i className="fa-solid fa-thumbs-down"></i></div>    
+    <div className='panel_header_content' style={{ display:'flex',flexDirection:'row', justifyContent:'space-between'}}>
+        <div className='panel_header_content_title'>{props.name}</div>
+        <div className='panel_header_content_icon' style={{ display:'flex',flexDirection:'row', justifyContent:'space-between'}}>
+            <div className='mr-3'>15 <i className="fa-solid fa-thumbs-up fa-lg" style={{color:'rgb(237, 125, 49)'}}></i></div>    
+            <div className='mr-3'>5 <i className="fa-solid fa-thumbs-down fa-lg" style={{color:'rgb(112, 172, 72)'}}></i></div>    
         </div>
     </div>
   )
